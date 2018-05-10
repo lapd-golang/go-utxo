78f8de81e802ec69996d04f4160d6e8beaf1cca6
Terence Tsao <terence@prysmaticlabs.com>
2018-05-10 15:25:04 -0700

    main.go to print

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,19 +1,19 @@
 package blockchain
 
 type Blockchain struct {
-	blocks []*Block
+	Blocks []*Block
 }
 
 func (bc Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks) - 1]
+	prevBlock := bc.Blocks[len(bc.Blocks) - 1]
 	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	bc.Blocks = append(bc.Blocks, newBlock)
 }
 
 func NewGenesisBlock() *Block {
 	return NewBlock("first block", []byte{})
 }
 
-func newBlockChain() *Blockchain{
+func NewBlockChain() *Blockchain{
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"UTXO-in-Go/blockchain"
+)
+
+func main() {
+	bc := blockchain.NewBlockChain()
+
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 more BTC to Ivan")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
