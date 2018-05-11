01826cd272e334267fe9128fbcd805e5b1df1431
Terence Tsao <terence@prysmaticlabs.com>
2018-05-11 16:05:09 -0700

    serialize block data

---
+++ blockchain/encode.go
@@ -0,0 +1,15 @@
+package blockchain
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	encoder.Encode(b)
+
+	return result.Bytes()
+}
