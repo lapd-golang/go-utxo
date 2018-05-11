584f07071374336cc1e0e101e9f84328cc670003
Terence Tsao <terence@prysmaticlabs.com>
2018-05-10 18:28:59 -0700

    prepare data for pow

--- blockchain/pow.go
+++ blockchain/pow.go
@@ -1,6 +1,9 @@
 package blockchain
 
-import "math/big"
+import (
+	"math/big"
+	"bytes"
+)
 
 const targetBits = 24
 
@@ -13,4 +16,17 @@ func NewPOW(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-targetBits))
 	return &ProofOfWork{b, target}
+}
+
+func (pow ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{})
+	return data
 }
