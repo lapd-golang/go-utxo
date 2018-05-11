9066c4a938c6ae7e215bf9cb370933734ce1b928
Terence Tsao <terence@prysmaticlabs.com>
2018-05-11 15:52:12 -0700

    add nonce to block structure

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,8 +2,8 @@ package blockchain
 
 import (
 	"bytes"
-	"strconv"
 	"crypto/sha256"
+	"strconv"
 	"time"
 )
 
@@ -12,6 +12,7 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
 func (b *Block) SetHash() {
@@ -23,7 +24,10 @@ func (b *Block) SetHash() {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewPOW(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
