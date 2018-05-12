452b3c922f0b21aa7a9f586684f7b35d438d634c
Terence Tsao <terence@prysmaticlabs.com>
2018-05-12 16:02:25 -0700

    bolt db with NewBlockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,11 +1,13 @@
 package blockchain
 
+import bolt
+
 type Blockchain struct {
 	Blocks []*Block
 }
 
 func (bc Blockchain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks) - 1]
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.Blocks = append(bc.Blocks, newBlock)
 }
@@ -14,6 +16,27 @@ func NewGenesisBlock() *Block {
 	return NewBlock("first block", []byte{})
 }
 
-func NewBlockChain() *Blockchain{
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+func NewBlockChain() *Blockchain {
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
