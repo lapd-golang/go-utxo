2cb8c3f519b5fabf8f3bcf5395822a22dbc352ac
Terence Tsao <terence@prysmaticlabs.com>
2018-05-14 09:46:05 -0700

    cli print chain

--- blockchain/cli.go
+++ blockchain/cli.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"fmt"
+	"strconv"
 )
 
 type CLI struct {
@@ -44,4 +45,23 @@ func (cli *CLI) Run() {
 func (cli *CLI) addBlock(data string) {
 	cli.bc.AddBlock(data)
 	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewPOW(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
 }
