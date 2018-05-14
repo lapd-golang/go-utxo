5e8b00ee68ecf956dc80789e6d819569177b87cf
Terence Tsao <terence@prysmaticlabs.com>
2018-05-14 09:44:51 -0700

    cli run function

--- blockchain/cli.go
+++ blockchain/cli.go
@@ -1,5 +1,41 @@
 package blockchain
 
+import (
+	"flag"
+	"os"
+)
+
 type CLI struct {
 	bc *Blockchain
+}
+
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		addBlockCmd.Parse(os.Args[2:])
+	case "printchain":
+		printChainCmd.Parse(os.Args[2:])
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
 }
