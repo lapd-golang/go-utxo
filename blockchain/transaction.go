0c16aa22a4d2e60b7b6879306bb43433503bfe24
Terence Tsao <terence@prysmaticlabs.com>
2018-05-16 13:35:07 -0700

    new coinbase transaction

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "fmt"
+
 type Transaction struct {
 	ID []byte
 	Vin []TXInput
@@ -15,4 +17,17 @@ type TXInput struct {
 	Txid []byte
 	Vout int
 	ScriptSig string
+}
+
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+
+	return &tx
 }
