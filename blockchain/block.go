35c7836a36bd0e3068c0421b4904610a75ff9d84
Terence Tsao <terence@prysmaticlabs.com>
2018-05-16 13:39:11 -0700

    get tx hashes

--- blockchain/block.go
+++ blockchain/block.go
@@ -31,3 +31,15 @@ func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
 	block.Nonce = nonce
 	return block
 }
+
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
