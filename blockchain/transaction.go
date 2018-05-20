fe7f056efd97d8199aec4cb5cc05e04022b93d03
Terence Tsao <terence@prysmaticlabs.com>
2018-05-20 14:15:52 -0700

    tx can use output

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -13,8 +13,14 @@ type Transaction struct {
 }
 
 type TXOutput struct {
-	Value int
-	ScriptPubKey string
+	Value      int
+	PubKeyHash []byte
+}
+
+func (out *TXOutput) Lock(address []byte) {
+	pubKeyHash := Base58Decode(address)
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
+	out.PubKeyHash = pubKeyHash
 }
 
 type TXInput struct {
